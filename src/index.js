25571ad3a2516a37a92d7e72ed3791e2f94a4882
Maksim <honestliar91@gmail.com>
2021-07-15 13:21:07 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const pairs = {},
+      strArr = str.split(''),
+      stek = [];
+  let bool = true;    
+  bracketsConfig.forEach(item => {
+    pairs[item[0]] = item[1];
+  });
+  strArr.forEach(item => {
+    if (item in pairs && (item != pairs[item] || !stek.length || item != stek[stek.length-1])) {
+      stek.push(item);
+    } else if (stek.length > 0 && item === pairs[stek[stek.length-1]]) {
+      stek.pop();
+    } else {
+      bool = false;
+    }
+  });
+  if (bool && stek.length == 0) {
+    return true;
+  } else { 
+    return false;
+  }
 }
